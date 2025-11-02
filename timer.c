9e7329e3357a14f21c546b96424d71c525404650
artyoomi <zuodleq@gmail.com>
2025-11-02 19:28:24 +0300

    Add code for common logic

--- timer.c
+++ timer.c
@@ -1,12 +1,13 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "task.h"
 
 int main(int argc, char* argv[])
 {
     MPI_Init(&argc, &argv);
-    
+
     // Get rank
     int world_rank = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
@@ -18,22 +19,22 @@ int main(int argc, char* argv[])
     int root_rank = 0;
 
     #ifdef MEASURE_TIME
-    MPI_Barrier();
-    double start_time, end_time;
-    if (world_rank == 0) {
-        start_time = MPI_Wtime();
+    MPI_Barrier(MPI_COMM_WORLD);
+    clock_t start_clock, end_clock;
+    if (world_rank == root_rank) {
+        start_clock = clock();
     }
     #endif
 
-    if (task(world_size, world_rank, root_rank)) {
+    if (task(world_size, world_rank, argc, argv)) {
         perror("Unable to recover from error in main task");
         goto finalize;
     }
-    
+
     #ifdef MEASURE_TIME
-    if (world_rank == 0) {
-        end_time = MPI_Wtime();
-        printf("%lf\n", end_time - start_time);    
+    if (world_rank == root_rank) {
+        end_clock = clock();
+        printf("%lf\n", ((double)end_clock - start_clock) / CLOCKS_PER_SEC);
     }
     #endif
 
